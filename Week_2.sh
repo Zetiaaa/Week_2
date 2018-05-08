4e4cf2c7793f682ff409bbfba41650053f6366d3
Zetiaaa <35321054+Zetiaaa@users.noreply.github.com>
2018-05-07 21:43:26 -0500

    Create Week_2.sh

---
+++ Week_2.sh
@@ -0,0 +1,16 @@
+#bin/bash
+echo hello world
+cat /etc/passwd
+touch file1
+touch file2
+touch file3
+touch file4
+cat file1 file2
+ls -l
+cp file1 file2
+ls-l
+mv file1 file3
+ls -l
+echo hello again
+
+
